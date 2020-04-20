3b8f0992c881d005827cda20e6b8c9341e5d080b
noelspa37 <noel.platzi.spa.37@gmail.com>
2020-04-19 22:46:58 -0500

    Version 2 - fin curso

---
+++ src/components/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/Categories.scss';
+
+const Categories = ({ children, title }) => {
+  return (
+    <div className='Categories'>
+      <h3 className='categories__title'>{title}</h3>
+      {children}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/containers/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initalState';
+
+const App = () => {
+  const videos = useInitialState(API);
+
+  return (
+    <div>
+      <Header />
+      <Search />
+
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            {videos.mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
+          </Carousel>
+        </Categories>
+      )}
+
+      <Categories title='Tendencias'>
+        <Carousel>
+          {videos.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Categories title='Orignales de Platzi Video'>
+        <Carousel>
+          {videos.originals.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
